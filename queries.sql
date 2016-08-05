bb50ed09c785f31975eae70470631020616fa664
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-08-05 16:16:18 -0400

    -Created an SQL view

--- queries.sql
+++ queries.sql
@@ -20,4 +20,13 @@ INSERT INTO StudentSemester (uid, sid, sectionName)
     WHERE s.uid NOT IN (
       SELECT st.uid
       FROM StudentSemester st)
-  )
+  );
+
+# This shows students who are registered in a semester and are in a group
+CREATE VIEW RegisteredStudentsInGroup AS
+
+  SELECT u.*, gt.* FROM Students s LEFT JOIN Users u ON u.uid = s.uid , Groups gt WHERE s.uid IN
+                                                                                        (SELECT st.uid FROM StudentSemester st where st.uid IN
+                                                                                                                                     (SELECT g.uid FROM GroupMembers g WHERE g.gid IN
+                                                                                                                                                                             (SELECT gr.gid FROM Groups gr WHERE gt.gid = gr.gid)))
+# The view can be used:  SELECT * FROM RegisteredStudentsInGroup
