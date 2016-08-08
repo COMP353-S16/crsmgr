7b1c9a084fb2db8d5439331015f9e49696c831c9
Dimitri Topaloglou <dtopaloglou@gmail.com>
2016-08-08 02:08:30 -0400

    -added db structure in sql file.again.

--- roc353.sql
+++ roc353.sql
@@ -1,14 +1,13 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 07, 2016 at 11:43 PM
+-- Server version: 5.5.45-cll-lve
+-- PHP Version: 5.4.31
 
-  -- phpMyAdmin SQL Dump
-  -- version 4.0.10.14
-  -- http://www.phpmyadmin.net
-  --
-  -- Host: localhost:3306
-  -- Generation Time: Aug 07, 2016 at 11:43 PM
-  -- Server version: 5.5.45-cll-lve
-  -- PHP Version: 5.4.31
-
-  SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
